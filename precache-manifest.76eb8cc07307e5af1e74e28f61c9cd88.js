cf5fa1f7ec412f3880c606587dcd6838e542b3a2
Code Sandbox <hello@codesandbox.io>
2022-04-29 12:26:33 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.76eb8cc07307e5af1e74e28f61c9cd88.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a94cd43165a82594a7360549d8f97af",
+    "url": "https://sergemarinfo.github.io/csb-zl7dil/index.html"
+  },
+  {
+    "revision": "5babe2661be3f5928454",
+    "url": "https://sergemarinfo.github.io/csb-zl7dil/static/css/2.cccfd250.chunk.css"
+  },
+  {
+    "revision": "5babe2661be3f5928454",
+    "url": "https://sergemarinfo.github.io/csb-zl7dil/static/js/2.0c9eb138.chunk.js"
+  },
+  {
+    "revision": "40caab17c0a12517f481",
+    "url": "https://sergemarinfo.github.io/csb-zl7dil/static/js/main.199276f3.chunk.js"
+  },
+  {
+    "revision": "843e26407c2cab59a7d1",
+    "url": "https://sergemarinfo.github.io/csb-zl7dil/static/js/runtime~main.637633ce.js"
+  },
+  {
+    "revision": "5d295cc139e67f13edc871f07f75047f",
+    "url": "https://sergemarinfo.github.io/csb-zl7dil/static/media/blueprint-icons-16.5d295cc1.woff2"
+  },
+  {
+    "revision": "81ba2c29edca4323c5363c2113f4455d",
+    "url": "https://sergemarinfo.github.io/csb-zl7dil/static/media/blueprint-icons-16.81ba2c29.ttf"
+  },
+  {
+    "revision": "edafa1f4fc6228bf8797dd3262f71af4",
+    "url": "https://sergemarinfo.github.io/csb-zl7dil/static/media/blueprint-icons-16.edafa1f4.woff"
+  },
+  {
+    "revision": "ffd4027de345feee47ceb06bf779ae51",
+    "url": "https://sergemarinfo.github.io/csb-zl7dil/static/media/blueprint-icons-16.ffd4027d.eot"
+  },
+  {
+    "revision": "0144a62006a1c3fc23b1420e7fbbca61",
+    "url": "https://sergemarinfo.github.io/csb-zl7dil/static/media/blueprint-icons-20.0144a620.eot"
+  },
+  {
+    "revision": "3916b23bd8f0e47c28abb7f63ad55d6c",
+    "url": "https://sergemarinfo.github.io/csb-zl7dil/static/media/blueprint-icons-20.3916b23b.ttf"
+  },
+  {
+    "revision": "5014bff33a089bf8b6dddd85cde567b6",
+    "url": "https://sergemarinfo.github.io/csb-zl7dil/static/media/blueprint-icons-20.5014bff3.woff"
+  },
+  {
+    "revision": "60937408ab08cbf425490293d2f3b8b6",
+    "url": "https://sergemarinfo.github.io/csb-zl7dil/static/media/blueprint-icons-20.60937408.woff2"
+  }
+]);
